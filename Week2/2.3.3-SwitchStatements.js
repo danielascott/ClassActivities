4557a322f28cebc9e03c024ae3fb11808d56cd5e
Daniel Scott <danielascott@gmail.com>
2023-11-15 19:48:48 -0600

    Switch Case Statement

---
+++ Week2/2.3.3-SwitchStatements.js
@@ -0,0 +1,54 @@
+// Correct these statements so they make sense
+// Sunday = 0, Monday = 1, ... Saturday = 6
+// re-assign day to any number from 0 - 7 to test the result
+let day = 1;
+let dayName = `Today`;
+
+/**
+ * sunday = 0
+ * monday = 1
+ * tuesday = 2
+ * wed = 3
+ * thurs = 4
+ * fri = 5
+ * sat = 6
+ */
+
+
+
+
+// Complete this switch case statement to produce the correct results please.
+
+switch (day) {
+  case 3 :
+    dayName = `Today is Wednesday`;
+    break;
+  case 1:
+    dayName = `Today is Monday`;
+    break;
+
+  // Note the code blocks in the next 2 cases: Why?
+  case 4: {
+    const message = `Today is Thursday`;
+    console.log(message);
+    break;
+  }
+  case 5 : {
+    const message = `Tomorrow it's the weekend!`;
+    console.log(message);
+    break;
+  }
+  default:
+    console.log(grab a cold one);
+    break;
+  case 2 :
+    console.log(`Today is Tuesday`);
+    break;
+  // write a case here for the weekend (how many days in the weekend?)
+  case 6:
+    alert(`It's the Weekend !!`);
+
+}
+if (dayName !== 'Today'){
+console.log(dayName);
+}
