17147d860c5d7e1632762442340ed7eeb31ad795
Daniel Scott <danielascott@gmail.com>
2023-11-20 21:31:29 -0600

    Parameters

---
+++ Week3/3.1.4-Parameters.js
@@ -0,0 +1,40 @@
+// Named Parameters
+// write a function that takes two named parameters:
+// print each named parameter,
+// then return the parameters added together
+
+function myFunction(param1, param2) {
+    console.log(param1)
+    console.log(param2)
+    return param1 + param2;
+}
+
+// invoke the function and pass in two numbers
+
+myFunction(32,43)
+
+// invoke the function and pass in more than two numbers
+
+myFunction(44,35,63,23)
+
+// invoke the function and pass in only one number
+
+myFunction(4)
+
+// change the function to set default values for the parameters
+
+function myFunction(param1 = 4, param2 = "apple") {
+    console.log(param1)
+    console.log(param2)
+    return param1 + param2;
+}
+
+// again, invoke the function and pass in only one number
+
+myFunction(6)
+
+// Rest Operator
+// add a rest operator to the function's parameters
+// add a line to the function's body to print the value of the rest operator
+
+// again, invoke the function and pass in more than two numbers
