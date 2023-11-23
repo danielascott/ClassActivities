6329c04d0c890b3531aead67cc1f01d275e64d6c
Daniel Scott <danielascott@gmail.com>
2023-11-22 18:56:40 -0600

    Arrays

---
+++ Week3/3.2.1-Arrays.js
@@ -0,0 +1,21 @@
+// create an Array using an Array literal
+
+const trucks = ["ford", "chevy", "dodge"];
+
+// access the 1st item in the Array
+
+console.log(trucks[0]);
+
+// access the last item in the Array
+
+console.log(trucks[2]);
+
+// print the length of the Array
+
+trucks.length;
+
+// use the length property to access the last item in the Array
+
+console.log(trucks[trucks.length - 1]);
+
+// with for...of, loop over the Array, modify the value and add to a different Array
