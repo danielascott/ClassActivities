97da108235368ee54fe553dada694a091cc056e8
Daniel Scott <danielascott@gmail.com>
2023-11-22 19:30:57 -0600

    added explanation

--- Week3/3.2.1-Arrays.js
+++ Week3/3.2.1-Arrays.js
@@ -1,6 +1,6 @@
 // create an Array using an Array literal
 
-const trucks = ["ford", "chevy", "dodge"];
+const trucks = ["f-450", "f-250", "dodge"];
 
 // access the 1st item in the Array
 
@@ -19,3 +19,11 @@ trucks.length;
 console.log(trucks[trucks.length - 1]);  // essentially 3 - 1 , which shows the dodge because dodge is technically 2
 
 // with for...of, loop over the Array, modify the value and add to a different Array
+
+const cars = [];
+
+for (let vehicle of trucks) {
+    let output = "ford ${vehicle}"; // this will put ford infront of the arrays .. f450, f250, etc. 
+    cars.push(output);
+}
+
