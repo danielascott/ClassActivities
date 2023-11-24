dac6704370008a04f913181fd90dcf81e15c284e
Daniel Scott <danielascott@gmail.com>
2023-11-24 14:05:48 -0600

    Updated from instructor

--- Week3/3.1.7-ArrowSyntax.js
+++ Week3/3.1.7-ArrowSyntax.js
@@ -1,23 +1,58 @@
 // refactor the functions below into arrow syntax
-function myNumberFunction() {
-  return ((2 ** 2 + 3) % 4) * 14;
-}
 
-function greet(name) {
-  return `Hello, ${name}`;
-}
+// Function Declaration
+// function myNumberFunction() {
+//   return ((2 ** 2 + 3) % 4) * 14;
+// }
 
-const timeOfDayGreet = function(name, timeOfDay) {
+// Arrow Function
+const myNumberFunction = () => ((2 ** 2 + 3) % 4) * 14;
+console.log(myNumberFunction());
+
+// Function Declaration
+// function greet(name) {
+//   return `Hello, ${name}`;
+// }
+
+// Arrow Function
+const greet = name => `Hello, ${name}`;
+console.log(greet("Michael"));
+
+// Function Declaration
+// const timeOfDayGreet = function(name, timeOfDay) {
+//   return `Hello, ${name}, good ${timeOfDay}`;
+// };
+
+// Arrow Function
+const timeOfDayGreet = (name, timeOfDay) => {
   return `Hello, ${name}, good ${timeOfDay}`;
 };
+console.log(timeOfDayGreet("Michael", "Morning"));
 
-function tripleAndHalf(num) {
+// Function Declaration
+// function tripleAndHalf(num) {
+//   let triple = num * 3;
+//   return triple / 2;
+// }
+
+// Arrow Function
+const tripleAndHalf = num => {
   let triple = num * 3;
   return triple / 2;
-}
+};
+console.log(tripleAndHalf(10));
 
-function sumTwoNumbers(num1, num2) {
+// Function Declaration
+// function sumTwoNumbers(num1, num2) {
+//   let sum = num1 + num2;
+//   console.log(`The sum of your numbers is ${sum}.`);
+//   return sum;
+// }
+
+// Arrow Function
+const sumTwoNumbers = (num1, num2) => {
   let sum = num1 + num2;
   console.log(`The sum of your numbers is ${sum}.`);
   return sum;
-}
+};
+console.log(sumTwoNumbers(10, 20));
